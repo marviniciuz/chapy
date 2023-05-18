a40458fa3447584b02cd582151289e89b3a96731
Marcus Vinicius <eumarcusviniciusp@gmail.com>
2023-05-18 11:29:11 -0300

    feat: add socket.io

--- src/chat.py
+++ src/chat.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_socketio import SocketIO,emit
+
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route("/")
 def hello():
     return open('index.html').read()
 
+@socketio.on('message')
+def messagereceived(data):
+    emit('message',data)
+
 if __name__ == '__main__':
-    app.run('127.0.0.1',3000)
+    socketio.run(app,'127.0.0.1',3000)
